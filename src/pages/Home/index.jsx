accf937aa97bff64103278336a22f4c935e2c671
Made Aryadi <madearyadi@gmail.com>
2020-07-16 22:16:49 +0700

    create font component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Container from '../../components/container/Container'
 import Image from '../../components/image/Image'
+import Title from '../../components/title/Title'
 
 function Home() {
   return (
@@ -8,6 +9,11 @@ function Home() {
       <h1>Home</h1>
       <Container>
         <Image />
+        <Title style={{marginLeft: 10}}>Lorem Ipsum</Title>
+      </Container>
+      <Container>
+        <Image />
+        <Title style={{marginLeft: 10}}>Lorem Ipsum</Title>
       </Container>
     </>
   )
